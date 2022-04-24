9146ca209027013f860df9b30ba71e85aa35a555
krishpy3 <98233937+krishpy3@users.noreply.github.com>
2022-04-24 19:53:01 +0530

    Update Function.cs

--- myDotnet/src/myDotnet/Function.cs
+++ myDotnet/src/myDotnet/Function.cs
@@ -4,7 +4,7 @@ using Amazon.Lambda.Core;
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 
-namespace myDotnet;
+namespace myDotnet
 {
     public class Function
     {
