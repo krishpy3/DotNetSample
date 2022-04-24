00450bc56b2b129ac3237c88330f889674d2506b
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2022-04-24 19:43:32 +0530

    Update Function.cs

--- myDotnet/src/myDotnet/Function.cs
+++ myDotnet/src/myDotnet/Function.cs
@@ -4,18 +4,19 @@ using Amazon.Lambda.Core;
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace myDotnet;
-
-public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public class Function
     {
-        return input.ToUpper();
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input.ToUpper();
+        }
     }
 }
